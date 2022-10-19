e01319e86f46848ea506f592ed172e72899edb30
Spider Man <tranvandathtml0503@gmail.com>
2022-10-19 12:17:50 +0700

    Code splitting lazy load component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import { Fragment } from "react";
+import React, { Fragment, Suspense } from "react";
 import "swiper/scss";
 import { Routes, Route } from "react-router-dom";
 import Main from "./components/layout/Main";
 import BannerList from "./components/banner/BannerList";
-import HomePage from "./pages/HomePage";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+// import HomePage from "./pages/HomePage";
+// import MoviePage from "./pages/MoviePage";
+// import MovieDetailsPage from "./pages/MovieDetailsPage";
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const MoviePage = React.lazy(() => import("./pages/MoviePage"));
+const MovieDetailsPage = React.lazy(() => import("./pages/MovieDetailsPage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route path="/" element={<Main></Main>}>
+      <Suspense fallback={<div>Loading.....</div>}>
+        <Routes>
+          <Route path="/" element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <BannerList></BannerList>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movies/:movieID"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
           <Route
-            path="/"
-            element={
-              <>
-                <BannerList></BannerList>
-                <HomePage></HomePage>
-              </>
-            }
+            path="*"
+            element={<h1 className="text-white text-[60px]">Error 404</h1>}
           ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movies/:movieID"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-        <Route
-          path="*"
-          element={<h1 className="text-white text-[60px]">Error 404</h1>}
-        ></Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
